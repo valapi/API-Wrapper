ac80dfa1c05f3e18b5f22f0f35b3e85d1e09c21e
KT_NG3 <84909754+KTNG-3@users.noreply.github.com>
2022-05-08 16:49:52 +0700

    1.0.0

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-export { WrapperClient } from "./client/Client";
+export { WrapperClient as Client } from "./client/Client";
 export type { ValWrapperClient, ValWrapperClientConfig, ValWrapperClientError, ValWrapperClientEvent, ValWrapperClientPlatfrom, ValWrapperConfig } from "./client/Client";
 export type { ValWrapperAxios, ValWrapperAxiosEvent, ValWrapperAxiosError, ValWrapperAxiosMethod, ValWrapperAxiosRequest } from "./client/AxiosClient";
 export type { ValWrapperAuth } from "./auth/Account";
--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.WrapperClient = void 0;
+exports.Client = void 0;
 var Client_1 = require("./client/Client");
-Object.defineProperty(exports, "WrapperClient", { enumerable: true, get: function () { return Client_1.WrapperClient; } });
+Object.defineProperty(exports, "Client", { enumerable: true, get: function () { return Client_1.WrapperClient; } });
 //# sourceMappingURL=index.js.map

75dc67929849a5c727d13e1a64021931025c69cb
KT_NG3 <84909754+KTNG-3@users.noreply.github.com>
2022-06-18 16:20:09 +0700

    reload

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 1.2.4
+
+- fixed
+
 # 1.0.0 (1 January, 2020)
 
 - Initial release
--- dist/client/Client.js
+++ dist/client/Client.js
@@ -95,10 +95,13 @@ class WrapperClient extends lib_1.ValEvent {
         }
         this.RegionServices = new lib_2.ValRegion(this.region.live).toJSON();
         //request client
+        //cipher suite: https://developers.cloudflare.com/ssl/ssl-tls/cipher-suites/
         const ciphers = [
-            'TLS_CHACHA20_POLY1305_SHA256',
             'TLS_AES_128_GCM_SHA256',
             'TLS_AES_256_GCM_SHA384',
+            'TLS_AES_128_CCM_8_SHA256',
+            'TLS_AES_128_CCM_SHA256',
+            'TLS_CHACHA20_POLY1305_SHA256',
             'TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256'
         ];
         const _normalAxiosConfig = {
